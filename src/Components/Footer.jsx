b10b6333397caeafaedaf21ac0491781074bd3fc
syd-io <sy.isahd@gmail.com>
2025-02-10 13:22:10 -0500

    added page content and footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>&copy; Event Planner Organization. All rights reserved.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
